58aef82adb19199afaa488e11f246a56b140611a
John Solomon <jolomonson@gmail.com>
2026-01-17 17:08:09 +0100

    Fix api_version import error in the attendanceapi app

--- attendanceapi/urls.py
+++ attendanceapi/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from attendanceapi.api_views import recognize_frame, mark_attendance, health_check
+from attendanceapi.api_views import api_version
 
 urlpatterns = [
     path("recognize-frame/", recognize_frame, name="recognize-frame"),
