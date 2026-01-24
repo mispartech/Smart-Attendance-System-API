6ceec77a4284bc53c771096cb00ce449ee763d40
John Solomon <jolomonson@gmail.com>
2026-01-24 18:53:43 +0100

    Delete .env file and add secret key directly

--- smartattendancesystemapi/settings.py
+++ smartattendancesystemapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-^v3uk0tza_al=41kk%h64(o%=c!4-ox8xm6hv2k+vj(8_*t7dl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "True"
