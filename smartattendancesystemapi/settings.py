6081cfda1918152c3333ffa0741fe47ece609536
John Solomon <jolomonson@gmail.com>
2026-01-24 17:24:35 +0100

    Update the Allowed Hosts in the settings file

--- smartattendancesystemapi/settings.py
+++ smartattendancesystemapi/settings.py
@@ -25,10 +25,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS",
-    ""
-).split(",")
+ALLOWED_HOSTS = [
+    "16.170.213.254",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
