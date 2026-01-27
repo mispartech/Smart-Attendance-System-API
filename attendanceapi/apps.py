c12c304c6319e0f76f93a62826bb0cb20f432786
John Solomon <jolomonson@gmail.com>
2026-01-27 01:06:42 +0100

    chore: update apps.py (Django app config changes)

--- attendanceapi/apps.py
+++ attendanceapi/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AttendanceapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'attendanceapi'
+    name = "attendanceapi"
+
+    def ready(self):
+        from attendanceapi.services.face_model import get_face_app
+        get_face_app()
+        print("Face model initialized and ready.")
